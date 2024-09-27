55ed8a0c8f26bd4ac1dc75ec08c0d0c7a7deff1e
wislove <shuanglong.liao@changhong.com>
2024-09-27 11:38:14 +0800

    fix: 修复有期望灵兽还刷新的bug (#2)

--- src/game/mgr/PetMgr.js
+++ src/game/mgr/PetMgr.js
@@ -20,11 +20,12 @@ export default class PetMgr {
         this.petPoolData = []; //用于存放刷新出来的灵兽池子, [{isGet=false, petId=11401},{isGet=false, petId=11401},{isGet=false, petId=11401}]
 
         // 灵兽刷新愿望池子(默认为:应龙，鸾鸟和五大神话)
-        this.wishPets = [114001, 114007, 115001, 115002, 115003, 115004, 115005];
+        this.wishPets = [112005, 114001, 114007, 115001, 115002, 115003, 115004, 115005];
         this.lastAdRewardTime = 0;
 
         this.isProcessing = false;
         this.initialized = false;
+        this.refreshLock = false;
 
         LoopMgr.inst.add(this);
         RegistMgr.inst.add(this);
@@ -74,24 +75,24 @@ export default class PetMgr {
         if (t.ret === 0) {
             this.petPoolData = t.petPoolData;
             logger.info(`[灵兽管理] 本次灵兽刷新结果: ${this.petPoolData.map(item => DBMgr.inst.getLanguageWord(`Items-${item.petId}`)).join(',')}`);
-            this.initialized = true;
+            this.refreshLock = false;
         }
     }
 
     processReward() {
-        // 判断灵兽池子里是否有希望的灵兽
-        if (!this.petPoolData || !this.initialized) {
-            logger.debug("[灵兽管理] 灵兽数据暂未同步,本次不执行");
+        if (this.refreshLock) {
+            logger.debug(`[灵兽管理] 灵兽刷新结果未返回,暂不推送执行任务`);
             return;
         }
 
         const now = Date.now();
         if (this.freeRefreshTimes < this.MAX_FREE_REFRESH_NUM && now - this.lastAdRewardTime >= this.AD_REWARD_CD) {
+            this.refreshLock = true;
+
             const logContent = `[灵兽刷新] 还剩 ${this.MAX_FREE_REFRESH_NUM - this.freeRefreshTimes - 1} 次免费刷新`;
             AdRewardMgr.inst.AddAdRewardTask({ protoId: Protocol.S_PET_REFRESH_POOL, data: { isUseADTime: false, isFree: 1 }, logStr: logContent });
             this.lastAdRewardTime = now;
             this.freeRefreshTimes++;
-            this.initialized = false;
         }
     }
 
@@ -107,16 +108,15 @@ export default class PetMgr {
                 return;
             }
 
+            if (this.petPoolData.length == 0) {
+                return;
+            }
+
             // 如果有希望灵兽,则不刷新
             // TODO 有希望灵兽,自动抓捕
-            const hasWishPets = this.petPoolData.forEach(item => {
-                if (this.wishPets.includes(item.petId) && !item.isGet) {
-                    logger.info(`[灵兽管理] 有期望灵兽未抓捕，不执行免费刷新, 期望灵兽: ${DBMgr.inst.getLanguageWord(`Items-${item.petId}`)}`);
-                    return true;
-                }
-            });
-
-            if (hasWishPets) {   
+            const wishPets = this.petPoolData.find(item => this.wishPets.includes(item.petId) && !item.isGet);
+            if (wishPets) {
+                logger.info(`[灵兽管理] 有期望灵兽未抓捕，不执行免费刷新, 期望灵兽: ${DBMgr.inst.getLanguageWord(`Items-${wishPets.petId}`)}`);   
                 this.clear();
                 return;
             }
