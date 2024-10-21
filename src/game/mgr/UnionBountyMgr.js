8fa3ab1e4fee189e0e4f7f5888ea8197c23e19ac
wislove <shuanglong.liao@changhong.com>
2024-10-21 16:33:40 +0800

    fix: fix UnionBountyMgr fix not get reward not start

--- src/game/mgr/UnionBountyMgr.js
+++ src/game/mgr/UnionBountyMgr.js
@@ -3,6 +3,7 @@ import Protocol from "#game/net/Protocol.js";
 import logger from "#utils/logger.js";
 import LoopMgr from "#game/common/LoopMgr.js";
 import UnionMgr from '#game/mgr/UnionMgr.js';
+import DBMgr from "#game/common/DBMgr.js";
 
 export default class UnionBountyMgr {
     constructor() {
@@ -17,7 +18,6 @@ export default class UnionBountyMgr {
         this.myCart = null;                 // 自己的押送车
         this.CHECK_CD = 1000 * 60 * 10;     // 每次间隔时间
         this.lastCheckTime = 0;             // 上次检查时间
-        this.initialized = false            // 是否同步妖盟数据
     }
 
     static get inst() {
@@ -45,7 +45,7 @@ export default class UnionBountyMgr {
         GameNetMgr.inst.sendPbMsg(Protocol.S_UNION_BOUNTY_ENTRY_MAP, {});
     }
 
-    // 获取悬赏信息
+    // 获取悬赏信息（这里会同步多次，数据可能不一致）
     UnionBountyEnterMapResp(t) {
         this.bountyTimes = t.playerData.bountyTimes;
         this.helpTimes = t.playerData.helpTimes;
@@ -54,8 +54,7 @@ export default class UnionBountyMgr {
         this.myCart = t.myCart || null;
         this.worshipRedPoint = t.worshipRedPoint;
         this.repointRedPoint = t.repointRedPoint;
-        this.groupType = t.groupType
-        this.initialized = true;
+        this.groupType = t.groupType;
     }
 
     // 排行榜膜拜
@@ -65,19 +64,39 @@ export default class UnionBountyMgr {
         logger.info(`[妖盟悬赏] 排行榜膜拜已完成`)
     }
 
-    // 押镖界面
+    // 请求打开悬赏界面
     UnionBountyOpenBountyEventReq() {
         GameNetMgr.inst.sendPbMsg(Protocol.S_UNION_BOUNTY_OPEN_BOUNTY_EVENT, {});
     }
 
     // 押镖界面返回&启动押镖
-    UnionBountyOpenBountyEventResp(t) {
-        // TODO: 只做了普通押送，怪兽没有抓到数据需要等~
-        curConfigId = t.bountyInfo.curConfigId;
-        logger.info(`[妖盟悬赏] 开始押送悬赏`)
-        GameNetMgr.inst.sendPbMsg(Protocol.S_UNION_BOUNTY_DEAL_BOUNTY, { curConfigId: curConfigId, type: this.groupType });
-        this.repointRedPoint = true;
-        this.bountyTimes += 1;
+    async UnionBountyOpenBountyEventResp(t) {
+        // 玩家数据同步
+        this.bountyTimes = t.playerData.bountyTimes;
+        this.helpTimes = t.playerData.helpTimes;
+
+        //悬赏信息, 只做了普通押送，怪兽没有抓到数据需要等~
+        const curConfigId = t.bountyInfo.curConfigId;
+
+        // 判断是否完成，完成了领取奖励，未完成，则判断myCart在地图上有没有，没有就开始押送，有就继续等待完成
+        const finishReward = t.bountyInfo.finishReward;
+        if (finishReward) {
+            const reward = finishReward?.reward.spilt('|');
+            logger.info(`[妖盟悬赏] 悬赏完成,奖励领取,奖励内容:${reward.array.map(element => {
+                const [key, value] = element.spilt('=');
+                return `奖励名称: ${DBMgr.inst.getLanguageWord(`Items-${key}`)}, 数量:${value}`
+            })}`);
+
+            // 领取悬赏奖励
+            this.UnionBountyGetRewardEscortReq();
+            // sleep 1秒钟后继续
+        } else {
+            if (this.myCart == null && this.bountyTimes < 2) {
+                logger.info(`[妖盟悬赏] 开始押送悬赏`)
+                GameNetMgr.inst.sendPbMsg(Protocol.S_UNION_BOUNTY_DEAL_BOUNTY, { curConfigId: curConfigId, type: this.groupType });
+                this.bountyTimes += 1;
+            }
+        }
     }
 
 
@@ -89,36 +108,44 @@ export default class UnionBountyMgr {
 
     async loopUpdate() {
         if (this.isProcessing) return;
-        const now = Date.now();
-        if (now - this.lastCheckTime < this.CHECK_CD) return;
-        this.lastCheckTime = now
         this.isProcessing = true;
         try {
-            let inUnion = UnionMgr.inst.inUnion()
+            const inUnion = UnionMgr.inst.inUnion()
             if (!inUnion) {
                 logger.info("[妖盟悬赏] 未加入妖盟");
                 this.clear();
                 return;
             }
-            if (!this.initialized) {
-                // 未初始化悬赏信息，进入悬赏地图
-                this.UnionBountyEnterMapReq();
+
+            // 检查cd间隔
+            const now = Date.now();
+            if (now.getHours() < 8 || now.getHours() > 22) {
                 return;
             }
-            logger.info(`[妖盟悬赏] 开始检查悬赏进度`)
+            if (now - this.lastCheckTime < this.CHECK_CD) {
+                return;
+            }
+            this.lastCheckTime = now;
+
+            logger.info(`[妖盟悬赏] 开始检查妖盟悬赏进度`);
+            // 进入悬赏地图（已经10分钟一次了，没必要再初始化标识）
+            this.UnionBountyEnterMapReq();
+            // sleep 2秒确保同步数据准确
+            await new Promise(resolve => setTimeout(resolve, 2000));
 
             // 排行榜膜拜
             if (this.worshipRedPoint) {
-                this.UnionBountyWorshipReq()
+                this.UnionBountyWorshipReq();
+                // sleep 0.5秒
+                await new Promise(resolve => setTimeout(resolve, 500));
             }
-            // 押镖
-            if (this.myCart == null && this.bountyTimes < 2) {
-                this.UnionBountyOpenBountyEventReq()
-            }
-            // 领取悬赏奖励
-            if (this.repointRedPoint) {
-                this.UnionBountyGetRewardEscortReq()
-            }
+
+            // 不管条件，都请求打开悬赏栏
+            this.UnionBountyOpenBountyEventReq();
+            // sleep 2秒等待悬赏返回结果处理
+            await new Promise(resolve => setTimeout(resolve, 2000));
+
+
             // 加入悬赏队伍，可遇不可求~
             if (null != this.monsterList) {
                 // TODO: 加入悬赏队伍，可遇不可求
